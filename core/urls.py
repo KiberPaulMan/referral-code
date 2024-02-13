97e213ae0f98a6279fce00119dc4dd527dfef99c
PaulMan <32840889+KiberPaulMan@users.noreply.github.com>
2024-02-13 20:05:48 +0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
-temp_url = "http://127.0.0.1:8000/reg/5j5cxcuPZ6",
-
 urlpatterns = [
-    path('register/', views.register_user, name='register'),
-    path('reg/<str:uuid>/', views.register_user, name='referral-register'),
-    path('email-register/', views.register_by_email, name='register-by-email'),
+    path('api/v1/profile/register/', views.ProfileCreateAPIView.as_view()),
+    path('api/v1/profile/register/<str:uuid>/', views.ProfileCreateAPIView.as_view()),
 
-    path('profile/<int:pk>/', views.ProfileAPIView.as_view(), name='user-detail'),
-    path('profile/<int:pk>/code/', views.ProfileCodeAPIView.as_view(), name='update-or-delete-code'),
+    path('api/v1/profile/<int:pk>/', views.ProfileAPIView.as_view()),
+    path('api/v1/profile/<int:pk>/code/', views.ProfileCodeAPIView.as_view()),
 ]
